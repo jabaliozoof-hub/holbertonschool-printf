80b2cbaefd3648888e7baff2d0b9cc1651abb584
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-08-05 20:15:16 +0300

    this is to add funtion is printf

--- main.h
+++ main.h
@@ -4,6 +4,14 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+
+typedef struct handler
+{
+	char spec;
+	int (*func)(va_list);
+} handler_t;
+
+int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_percent(void);
 int print_string(va_list args);
